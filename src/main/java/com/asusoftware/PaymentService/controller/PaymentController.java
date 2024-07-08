d1b5f9004f63476b3215dadc80af071dab506460
AsuSoftware <asu.software.dev@gmail.com>
2024-07-08 15:20:33 +0300

    Initial commit

--- src/main/java/com/asusoftware/PaymentService/controller/PaymentController.java
+++ src/main/java/com/asusoftware/PaymentService/controller/PaymentController.java
@@ -1,9 +1,29 @@
 package com.asusoftware.PaymentService.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.asusoftware.PaymentService.model.PaymentRequest;
+import com.asusoftware.PaymentService.model.PaymentResponse;
+import com.asusoftware.PaymentService.service.PaymentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
+
+    @Autowired
+    private PaymentServiceImpl paymentService;
+
+    @PostMapping
+    public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) {
+        return new ResponseEntity<>(
+                paymentService.doPayment(paymentRequest), HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable(name = "orderId") long orderId) {
+        return new ResponseEntity<>(paymentService.getPaymentDetailsByOrderId(orderId), HttpStatus.OK);
+    }
 }
--- src/main/java/com/asusoftware/PaymentService/service/PaymentService.java
+++ src/main/java/com/asusoftware/PaymentService/service/PaymentService.java
@@ -1,4 +1,10 @@
 package com.asusoftware.PaymentService.service;
 
+import com.asusoftware.PaymentService.model.PaymentRequest;
+import com.asusoftware.PaymentService.model.PaymentResponse;
+
 public interface PaymentService {
+    long doPayment(PaymentRequest paymentRequest);
+
+    PaymentResponse getPaymentDetailsByOrderId(long orderId);
 }
